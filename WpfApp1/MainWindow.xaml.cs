0696860aa34d581a141800e2d190a11cd6ca142e
sgbogelund@gmail.com <sgbogelund@gmail.com>
2022-04-06 13:39:23 +0200

    mode activated!

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -20,11 +20,12 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        Random random = new Random();
+        Random random = new();
         bool cheatMode = false;
         bool easyMode = false;
+        bool hardMode = false;
         double r, g, b = 0;
-        
+
         public MainWindow()
         {
             InitializeComponent();
@@ -35,23 +36,61 @@ namespace WpfApp1
         {
 
             Point Point = e.GetPosition(this);
-            
+
             double height = mainWindow.ActualHeight;
             double width = mainWindow.ActualWidth;
 
-            r = 255-((Point.X/width)*255);
-            g = ((Point.Y/height)*255);
-  
-            updateWindow();
+
+            if (easyMode)
+            {
+                if ((int)((Point.X / width) * 255) % 5.0 == 0)
+                {
+                    r = 255 - ((Point.X / width) * 255) + 1;
+                }
+                if ((int)((Point.Y / height) * 255) % 5.0 == 0)
+                {
+                    g = ((Point.Y / height) * 255);
+                }
+
+            }
+            else
+            {
+                r = 255 - ((Point.X / width) * 255);
+                g = ((Point.Y / height) * 255);
+            }
+
+            UpdateWindow();
 
         }
 
-        private void colorButton_Click(object sender, RoutedEventArgs e)
+        private void ColorButton_Click(object sender, RoutedEventArgs e)
         {
-            canvas.Background = new SolidColorBrush(Color.FromRgb((byte)(255 * random.NextDouble()), (byte)(255 * random.NextDouble()), (byte)(255 * random.NextDouble())));
+            byte red, green, blue;
+            red = (byte)random.Next(0, 255);
+            green = (byte)random.Next(0, 255);
+            blue = (byte)random.Next(0, 255);
+            if (easyMode)
+            {
+
+                while (red % 5.0 != 0)
+                {
+                    red++;
+                }
+                while (green % 5.0 != 0)
+                {
+                    green++;
+                }
+                while (blue % 5.0 != 0)
+                {
+                    blue++;
+                }
+            }
+
+            canvas.Background = new SolidColorBrush(Color.FromRgb(red, green, blue));
+
         }
 
-        private void easyButton_Click(object sender, RoutedEventArgs e)
+        private void EasyButton_Click(object sender, RoutedEventArgs e)
         {
             easyMode = !easyMode;
             if (easyMode)
@@ -62,11 +101,11 @@ namespace WpfApp1
             {
                 easyButton.Content = "Easy Mode Off";
             }
-            updateWindow();
+            UpdateWindow();
 
         }
 
-        private void cheatButton_Click(object sender, RoutedEventArgs e)
+        private void CheatButton_Click(object sender, RoutedEventArgs e)
         {
             cheatMode = !cheatMode;
             if (cheatMode)
@@ -77,26 +116,67 @@ namespace WpfApp1
             {
                 cheatButton.Content = "Activate Cheat";
             }
-            updateWindow();
+            UpdateWindow();
         }
 
-        private void mainWindow_MouseWheel(object sender, MouseWheelEventArgs e)
+        private void MainWindow_MouseWheel(object sender, MouseWheelEventArgs e)
         {
             double delta = (double)e.Delta;
             if (delta > 0)
             {
-                b++;
+                if (easyMode)
+                {
+                    if ((byte)b % 5.0 == 0)
+                    {
+                        b += 5;
+                    }
+                    else
+                    {
+                        b++;
+                    }
+                }
+                else
+                {
+                    b++;
+                }
             }
             else if (delta < 0)
             {
-                b--;
+                if (easyMode)
+                {
+                    if ((byte)b % 5.0 == 0)
+                    {
+                        b -= 5;
+                    }
+                    else
+                    {
+                        b--;
+                    }
+                }
+                else
+                {
+                    b--;
+                }
             }
-            updateWindow();
-
-
+            UpdateWindow();
         }
 
-        private void updateWindow()
+        private void hardButton_Click(object sender, RoutedEventArgs e)
+        {
+            hardMode = !hardMode;
+            if (hardMode)
+            {
+                hardButton.Content = "HARDMODE ON!!!";
+                BlackCanvas.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BlackCanvas.Visibility = Visibility.Hidden;
+                hardButton.Content = "hardMode ?";
+            }
+        }
+
+        private void UpdateWindow()
         {
             if (cheatMode)
             {
@@ -107,27 +187,31 @@ namespace WpfApp1
             {
                 rgbinfo.Text = $"R={(byte)r} G = {(byte)g} B = {(byte)b}";
             }
+
             mainWindow.Background = new SolidColorBrush(Color.FromRgb((byte)r, (byte)g, (byte)b));
-            checkWin();
+            CheckWin();
         }
 
-        private void checkWin()
+        private void CheckWin()
         {
+            int difficultyCoeficient = 15;
             if ((
                 ((SolidColorBrush)mainWindow.Background).Color.R == ((SolidColorBrush)canvas.Background).Color.R &&
                 ((SolidColorBrush)mainWindow.Background).Color.G == ((SolidColorBrush)canvas.Background).Color.G &&
                 ((SolidColorBrush)mainWindow.Background).Color.B == ((SolidColorBrush)canvas.Background).Color.B)
                 ||
                 (easyMode &&
-               ((((SolidColorBrush)mainWindow.Background).Color.R + 10 >= ((SolidColorBrush)canvas.Background).Color.R) &&
-               (((SolidColorBrush)mainWindow.Background).Color.R - 10 <= ((SolidColorBrush)canvas.Background).Color.R) &&
-               (((SolidColorBrush)mainWindow.Background).Color.G + 10 >= ((SolidColorBrush)canvas.Background).Color.G) &&
-               (((SolidColorBrush)mainWindow.Background).Color.G - 10 <= ((SolidColorBrush)canvas.Background).Color.G) &&
-               (((SolidColorBrush)mainWindow.Background).Color.B + 10 >= ((SolidColorBrush)canvas.Background).Color.B) &&
-               (((SolidColorBrush)mainWindow.Background).Color.B - 10 <= ((SolidColorBrush)canvas.Background).Color.B))))
+               (((SolidColorBrush)mainWindow.Background).Color.R + difficultyCoeficient >= ((SolidColorBrush)canvas.Background).Color.R) &&
+               (((SolidColorBrush)mainWindow.Background).Color.R - difficultyCoeficient <= ((SolidColorBrush)canvas.Background).Color.R) &&
+               (((SolidColorBrush)mainWindow.Background).Color.G + difficultyCoeficient >= ((SolidColorBrush)canvas.Background).Color.G) &&
+               (((SolidColorBrush)mainWindow.Background).Color.G - difficultyCoeficient <= ((SolidColorBrush)canvas.Background).Color.G) &&
+               (((SolidColorBrush)mainWindow.Background).Color.B + difficultyCoeficient >= ((SolidColorBrush)canvas.Background).Color.B) &&
+               (((SolidColorBrush)mainWindow.Background).Color.B - difficultyCoeficient <= ((SolidColorBrush)canvas.Background).Color.B)))
 
             {
                 MessageBox.Show("You WIN!");
+                canvas.Background = new SolidColorBrush(Color.FromRgb((byte)(255 * random.NextDouble()), (byte)(255 * random.NextDouble()), (byte)(255 * random.NextDouble())));
+                UpdateWindow();
             }
         }
     }
